acdc4bf0694c9f54649fe3e97b3440e2a860cb51
Ivo <ivnachev13@gmail.com>
2025-07-02 13:39:17 +0300

    Delete Java  programmes/numbers programme/src/FunctionalityEight.java

--- Java  programmes/numbers programme/src/FunctionalityEight.java
+++
@@ -1,9 +0,0 @@
-public class FunctionalityEight {
-    public static void eightFunctionality(int []arr) {
-
-        for (int i = arr.length-1; i>=0; i--) {
-
-            System.out.println("arr["+(i+1)+"]= "+arr[i]);
-        }
-    }
-}
